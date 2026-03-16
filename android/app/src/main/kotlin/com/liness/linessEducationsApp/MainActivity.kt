d5cbdc97f0cf13a7ba1b9ba6c3360cd3bd145c63
MohamedKhaled8 <mohamedkhaled9002432@gmail.com>
2026-03-16 05:01:45 +0200

    update new new

--- android/app/src/main/kotlin/com/liness/linessEducationsApp/MainActivity.kt
+++ android/app/src/main/kotlin/com/liness/linessEducationsApp/MainActivity.kt
@@ -9,44 +9,5 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-    private val CHANNEL = "com.liness.linessEducationApp/developer_settings"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
-    }
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "isDeveloperModeEnabled" -> {
-                    val isEnabled = isDeveloperModeEnabled()
-                    result.success(isEnabled)
-                }
-                "openDeveloperSettings" -> {
-                    openDeveloperSettings()
-                    result.success(null)
-                }
-                else -> {
-                    result.notImplemented()
-                }
-            }
-        }
-    }
-
-    private fun isDeveloperModeEnabled(): Boolean {
-        return Settings.Global.getInt(
-            contentResolver,
-            Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0
-        ) != 0
-    }
-
-    private fun openDeveloperSettings() {
-        val intent = Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
-        }
-    }
+   
 }
